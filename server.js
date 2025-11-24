72eb2ef2be534c52a7769307667ba60d71989eaf
Chanan Welt <chwelt88@gmail.com>
2025-11-25 00:25:20 +0200

    replace save endpoint with relevant post & put methods

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const corsOptions = {
   credentials: true,
 };
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions)); // for cross origin allowance
+app.use(express.json()); // for parsing application/json from frontend
+app.use(express.static("public")); // to serve frontend files from the 'public' folder after build
 
 //* ------------------- Bugs Crud -------------------
 //* List
@@ -26,20 +28,42 @@ app.get("/api/bug", async (req, res) => {
   }
 });
 
-//* Create/Update
-app.get("/api/bug/save", async (req, res) => {
-  // example request: http://localhost:3030/api/bug/save?_id=500qtl&title=foo&severity=5&createdAt=1761821492000&description=bar
+//* Update
+app.put("/api/bug/:bugId", async (req, res) => {
+  const { bugId } = req.params;
+  const { title, description, severity, labels } = req.body;
   const bugToSave = {
-    _id: req.query._id,
-    title: req.query.title,
-    description: req.query.description,
-    severity: +req.query.severity,
-    createdAt: +req.query.createdAt,
+    _id: bugId,
+    title,
+    description,
+    severity,
+    labels: [...labels],
   };
 
   try {
     const savedBug = await bugService.save(bugToSave);
     res.send(savedBug);
+  } catch (err) {
+    console.error(`Couldn't save bug ${bugToSave._id}`, err); // loggerService.error(`Couldn't save bug`, err);
+    res.status(400).send(`Couldn't save bug with id: ${bugToSave._id}`);
+  }
+});
+
+// * Create
+app.post("/api/bug/", async (req, res) => {
+  console.log(req.body);
+  const { title, description, severity, createdAt, labels } = req.body;
+
+  try {
+    const bugToSave = {
+      title,
+      description,
+      severity,
+      createdAt,
+      labels: [...labels],
+    };
+    const savedBug = await bugService.save(bugToSave);
+    res.status(201).send(savedBug);
   } catch (err) {
     console.error(`Couldn't save bug ${bugToSave._id}`, err); // loggerService.error(`Couldn't save bug`, err);
     res.status(400).send(`Couldn't save bug with id: ${bugToSave._id}`);
